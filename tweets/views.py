82cdfe555932ecd387858500a5675494badfd137
suesanjp <a.drop.of.my.essence@gmail.com>
2021-01-21 07:47:48 +0900

    Done Chapter 22

--- tweets/views.py
+++ tweets/views.py
@@ -2,6 +2,7 @@ import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
+from .forms import TweetForm
 from .models import Tweet
 
 # Create your views here.
@@ -10,6 +11,16 @@ from .models import Tweet
 def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1>Hello World</h1>")
     return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_create_view(request, *args, **kwargs):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        # do other form related logic
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/form.html', context={"form": form})
 
 
 def tweet_list_view(request, *args, **kwargs):
