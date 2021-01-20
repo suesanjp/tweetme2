22f7a9ce2a7d11a515f8852f352b2bd94e24c75c
suesanjp <a.drop.of.my.essence@gmail.com>
2021-01-20 22:29:36 +0900

    Done Chapter 20

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,4 @@
+import random
 from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
@@ -18,7 +19,8 @@ def tweet_list_view(request, *args, **kwargs):
     return json data
     """
     qs = Tweet.objects.all()
-    tweet_list = [{"id": x.id, "content": x.content} for x in qs]
+    tweet_list = [{"id": x.id, "content": x.content,
+                   "likes": random.randint(0, 99)} for x in qs]
     data = {
         "isUser": False,
         "response": tweet_list,
